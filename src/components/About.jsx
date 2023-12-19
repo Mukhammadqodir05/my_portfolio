7a305fd78a1c783bd69855db0af6effab82bdac5
Muhammadqodir <muhammadqodirummatov31@gmail.com>
2023-12-19 12:45:27 +0200

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import myImage from '/src/assets/myImage.jpg'
 import {Link} from 'react-scroll'
 import { FaGithub, FaTwitter, FaYoutube, FaLinkedin} from "react-icons/fa";
-
+import { Fade } from "react-awesome-reveal";
+import { Bounce } from 'react-awesome-reveal';
+import { Flip } from 'react-awesome-reveal';
+import { Reveal } from 'react-awesome-reveal';
+import { JackInTheBox } from 'react-awesome-reveal';
 
 const About = () => { 
 
@@ -10,11 +14,15 @@ const About = () => {
     <main name='about' className="second_color h-full md:h-[900px] md:pb-20 md:pt-[200px] pb-10 lg:h-screen flex w-full justify-center items-center p-3 lg:pt-40 pt-28 ">   
       <div className='lg:max-w-[1100px] w-full flex md:px-5 md:gap-10 lg:space-x-[-30px] lg:px-4 space-y-10 flex-col items-center text-center justify-center md:grid md:grid-cols-2  '>
         
+       <Fade duration={2000}>
          <div className='flex border-cyan-500 flex-col pb-10 my_image w-full max-w-[400px] md:max-w-[350px] lg:max-w-[400px] h-full max-h-[600px] border rounded-md p-4'>
-          <img className="md:max-w-[350px] lg:ml-2 flex items-center justify-center text-center max-h-[350px] p-4 rounded-tl-[8%] rounded-br-[8%] mt-[-10px]" src={myImage}/>
-           <div className='flex text-white flex-col flex-shrink-0 text-center justify-center px-2 '>
-                <h1 className='text-2xl'>𝑴𝒖𝒉𝒂𝒎𝒎𝒂𝒅𝒒𝒐𝒅𝒊𝒓</h1>
+          <Bounce>
+            <img className="md:max-w-[350px] lg:ml-2 flex items-center justify-center text-center max-h-[350px] p-4 rounded-tl-[8%] rounded-br-[8%] mt-[-10px]" src={myImage}/>
+          </Bounce>
 
+            <div className='flex text-white flex-col flex-shrink-0 text-center justify-center px-2 '>
+               <Fade cascade  duration={80} className='text-2xl'>𝑴𝒖𝒉𝒂𝒎𝒎𝒂𝒅𝒒𝒐𝒅𝒊𝒓</Fade> 
+            
               <div className="flex text-4xl justify-center mt-16 space-x-7 lg:space-x-9 md:text-5xl">
                   <a href="https://github.com/Muhammadqodir001" target="_blank" rel="noopener noreferrer" className="md:hover:scale-110 duration-300">
                     <FaGithub />
@@ -34,12 +42,13 @@ const About = () => {
                  <Link className='mt-[35px] md:mt-[50px] lg:mt-[20px] py-3' to='contact' spy={true} smooth={true}  duration={400}>
                      <button className='h1 rounded-xl py-3 px-10'>Contact me</button>
                 </Link>
-           
            </div>
-        </div>
+          </div>
+        </Fade>
           
+        <Fade duration={2000}>
             <div className='lg:max-w-[450px] max-w-[400px] text-left space-y-3 items-center flex flex-col border-cyan-500 '>
-                <h1 className='inline border-b-4 border-b-violet-600 md:mt-[-50px] font-extrabold text-center text-2xl'>☪️ 𝑺𝒆𝒍𝒇-𝑺𝒖𝒎𝒎𝒂𝒓𝒚 ☪️</h1>
+                <Fade cascade duration={70} className='inline border-b-4 border-b-violet-600 md:mt-[-50px] font-extrabold text-center text-2xl'>☪️ 𝑺𝒆𝒍𝒇-𝑺𝒖𝒎𝒎𝒂𝒓𝒚 ☪️</Fade>
                 <span className='font-bold'>𝐇𝐞𝐥𝐥𝐨! 𝐈'𝐦 𝐌𝐮𝐡𝐚𝐦𝐦𝐚𝐝𝐪𝐨𝐝𝐢𝐫, 𝐚 𝐩𝐚𝐬𝐬𝐢𝐨𝐧𝐚𝐭𝐞 𝐟𝐫𝐨𝐧𝐭-𝐞𝐧𝐝 𝐰𝐞𝐛 𝐝𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫.</span>
                  
                <span>My journey into the world of web development began in 2023 
@@ -66,6 +75,7 @@ const About = () => {
                      exceed expectations.
                </span>
           </div>
+          </Fade>
       </div>
     </main>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Snap from '/src/assets/Snap.png'
 import { Link } from 'react-scroll';
 import { Fade } from "react-awesome-reveal";
+import { JackInTheBox } from 'react-awesome-reveal';
 
 const Home = () => {
   
   return (
-    <main className='first_color flex justify-center items-center h-screen w-full pt-24 lg:pt-10 p-3'>
+    <main className='first_color flex justify-center items-center h-screen w-full pt-36 lg:pt-10 p-3'>
       <div className='flex md:px-2 lg:px-4 space-y-10 flex-col items-center text-center justify-center lg:grid lg:grid-cols-2 w-full'>   
   
-     <Fade >
-        <div className='flex flex-col justify-center items-center'>
-            <h1 className='hi flex font-extrabold md:text-7xl lg:text-7xl text-[#fff] md:pl-[90px]'>𝑯𝒊 𝒕𝒉𝒆𝒓𝒆👋🏻</h1>
+     <Fade cascade>
+        <ul className='flex flex-col justify-center items-center'>
+            <Fade cascade duration={70} className='hi flex font-extrabold md:text-7xl lg:text-7xl text-[#fff] md:pl-[90px]'>𝑯𝒊 𝒕𝒉𝒆𝒓𝒆👋🏻</Fade>
            <div className='flex flex-col mt-2 '>
               <span className='span text-2xl md:text-3xl text-start font-extrabold '>𝑰'𝒎 𝑴𝒖𝒉𝒂𝒎𝒎𝒂𝒅𝒒𝒐𝒅𝒊𝒓</span>
               <span className='span text-xl md:text-2xl text-start font-bold text-violet-500'>𝐖𝐞𝐛 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫</span>
@@ -25,15 +26,15 @@ const Home = () => {
                   <button className='h1  text-white rounded-md border p-2 w-[120px]'>𝑷𝒓𝒐𝒋𝒆𝒄𝒕𝒔 ✔️</button>
               </Link>
            </div>
-        </div>
+        </ul>
      </Fade>
 
-      <Fade>
+      <JackInTheBox>
           <div
              className='flex md:cursor-pointer justify-center'>
              <img className='img rounded-md border-gray-300 lg:max-w-[510px] md:max-w-[500px]' src={Snap} alt="" />
           </div>
-        </Fade>
+        </JackInTheBox>
 
       
       </div>
